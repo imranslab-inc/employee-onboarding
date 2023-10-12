011947c9743be15c34d456c05bbe040116ce5dc5
RootProgrammer <zamaan.md19@gmail.com>
2023-10-12 21:33:56 +0600

    day1 lecture.

---
+++ python/lectures/day1.py
@@ -0,0 +1,73 @@
+# basics of python
+# dynamically typed programming language unlike C/C++/Java are statically typed programming language
+
+# 1. comments
+# single line comment
+
+"""
+multi line comment
+
+line 1
+line 2
+line 3
+...
+
+NB: we can use triple quotes for multi line comment
+NB: never use single quotes for multi line comment
+NB: never use multi line comment/docstring at the beginning of the code
+"""
+
+
+# 2. print message
+print("your message here.")
+
+print("Hello Sushmita!")
+
+# formatting strings
+# f-string: f"strings.......{variable_name}.....strings......{variable_name}....."
+# f"hi, I am {name}. I am {age} years old. My school name is {school_name}"
+
+name = "sushmita"
+age = 20
+school_name = "cse"
+print(f"Hello {name}")
+
+# multi line string printing
+
+print(f"""
+    Hi,
+    I am {name}.
+    I am {age} years old.
+    My school name is {school_name}.
+    """)
+
+# 3. variables
+# information = data
+# variable = container of data
+
+# variable_name = value(data)
+
+name1 = "sushmita"
+
+
+
+"""
+Assignment:
+1. print your basci biodata using variables
+2. use both single and multi line string formatting for printing your biodata
+3. use day1.py in assignment folder for the assignment
+"""
+
+
+
+
+
+# 4. data types
+
+# operators
+
+# loops
+
+# functions
+
+# classes
