555aae8ac09819af590034f501ae3bfd0618611c
ByteBeaty <sushmitamitra404@gmail.com>
2023-10-14 15:04:55 +0600

    Ctrate basic biodata

--- python/assignments/day1.py
+++ python/assignments/day1.py
@@ -1,21 +1,64 @@
 # single line 
 
-name="Sushmita"
-company_name="imranslab"
-age=21
+name1="Sushmita"
+company_name2="imranslab"
+age3=21
 
 
 
-print (f"Hello! I am {name}")
-print (f"My age {age}")
-print (f"I am joining {company_name}")
+print (f"Hello! I am {name1}")
+print (f"My age {age3}")
+print (f"I am joining {company_name2}")
 
 
 
 print (f"""
 Hello! 
-I am {name}
-My age {age}
-I am joining {company_name}
+I am {name1}
+My age {age3}"
+I am joining {company_name2}
 
+""")
+
+
+
+
+# My biodata
+
+# Declearing variable
+
+name="Sushmita"
+contact_information="01620791555"
+date_of_birth="01/05/97"
+gender="female"
+nationality="Bangladeshi"
+maritial_status="married"
+languages_known="begali & English"
+company_name= "imranslab"
+
+#single line fuction call
+
+print (f"Hello! I am {name}")
+print(f"My contact number {contact_information}")
+print (f"My birth date {date_of_birth}")
+print(f"Gender {gender}")
+print(f"Nationality {nationality}")
+print(f"Maritial Staus {maritial_status}")
+print(f"Languages known {languages_known}")
+print(f"I joining {company_name}")
+
+
+#multi line function call
+
+print(f"""
+Hello!
+I am {name}
+My contact number {contact_information}
+My birth date {date_of_birth}      
+Gender {gender}
+Nationality {nationality}
+Maritial Staus {maritial_status}
+Languages known {languages_known}
+I joining {company_name}      
+      
 """)
